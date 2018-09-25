67fac0bfca9c78f378353108e32e2004d7d8fa0e
Jacques WILME <jacques.wilme@telecom-paristech.fr>
2018-09-25 11:46:07 +0200

    Ajout de quelques commentaires sur photo.h

--- photo.h
+++ photo.h
@@ -12,12 +12,21 @@ private:
   int longitude;
 
 public:
+  /**
+  *Constructeurs
+  */
   photo() : latitude(0), longitude(0) {}
   photo(string n, string p, int la, int lo) : multimedia(n,p), latitude(la), longitude(lo) {}
 
+  /**
+  *Getters et Setteurs de Latitude
+  */
   const int getLatitude() {return latitude;}
   void setLatitude(int _latitude) {latitude = _latitude;}
 
+  /**
+  *Getters et Settes de Longitude
+  */
   const int getLongitude() {return longitude;}
   void setLongitude(int _longitude) {longitude = _longitude;}
 
