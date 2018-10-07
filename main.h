8daf00fd2591a2f1400b2fa802edc3794b7ff544
Fost-68 <wilme.jacques@wanadoo.fr>
2018-10-07 13:43:54 +0200

    Ajout du fichier main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "multimedia.h"
+#include "video.h"
+#include "photo.h"
+#include "film.h"
+
+#endif
