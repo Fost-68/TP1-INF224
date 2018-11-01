62d72d8160dc81bd427b713ba6bf3835ee37490d
Fost-68 <wilme.jacques@wanadoo.fr>
2018-11-02 00:50:47 +0100

    Ajout de la javadoc sur les deux fichiers inclus dans ce commit

--- JavaSwing/src/ihm/TextPanel.java
+++ JavaSwing/src/ihm/TextPanel.java
@@ -1,20 +1,45 @@
 package ihm;
 
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.*;
 import java.awt.*;
 
+/**
+ *  @author WILME
+ *
+ *  This class represents a JPanel which contains a scrollable JTextArea
+ *
+ *
+ */
 public final class TextPanel extends JPanel{
-	
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The <i>JTextArea</i> which is contained in this JPanel
+     */
 	private JTextArea textArea;
-	
+
+    /**
+     * <b>Constructor</b> : Constructs the JPanel, the textArea and the scrollable Pane :
+     * Place the Component following a BorderLayout
+     */
 	public TextPanel() {
 	    this.setLayout(new BorderLayout());
-		textArea = new JTextArea();
+
+	    textArea = new JTextArea();
 		textArea.setSize(200, 200);
-		this.add(textArea, BorderLayout.CENTER);
+
+        JScrollPane scroller = new JScrollPane(textArea);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+		this.add(scroller, BorderLayout.CENTER);
 	}
-	
+
+    /**
+     * Display a message in the JTextArea
+     *
+     * @param msg <i>String</i> : Message to display in the Text Area
+     */
 	public void printText(String msg){
 	    textArea.append(msg);
 	}
--- JavaSwing/src/main/Main.java
+++ JavaSwing/src/main/Main.java
@@ -2,7 +2,20 @@ package main;
 
 import ihm.Frame;
 
+/**
+ * @author WILME
+ *
+ * Class containing the entry point of the program
+ */
 public class Main {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Entry point of the program
+     *
+     * @param args : <i>String []</i> String given by the user when loading the program
+     */
     public static void main(String [] args){
         new Frame();
     }
