f4c1cab0f6b6a62a86d5de097f17f77e52f85f9e
Fost-68 <wilme.jacques@wanadoo.fr>
2018-11-05 03:07:43 +0100

    WIP

--- swing/src/cs/Client.java
+++ swing/src/cs/Client.java
@@ -1,4 +1,22 @@
 package cs;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
+    private static final long serialVersionUID = 1L;
+    static final String DEFAULT_HOST = "localhost";
+    static final int DEFAULT_PORT = 3331;
+    private Socket sock;
+    private BufferedReader input;
+    private BufferedWriter output;
+
+    public Client() throws IOException {
+        sock = new Socket(DEFAULT_HOST, DEFAULT_PORT);
+        input = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        output = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+
+
+    }
 }
